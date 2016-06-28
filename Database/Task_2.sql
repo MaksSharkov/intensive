66211ad275e1f647831b01f9df0183953c86590f
SharkovMaksimUrievich <makssharkov@yandex.ru>
2016-06-28 15:04:40 +0300

    Update task SQL2

--- Database/Task_2.sql
+++ Database/Task_2.sql
@@ -1,24 +1,20 @@
 CREATE TABLE Projects
-	(	id int	PRIMARY KEY IDENTITY,
-		name nvarchar(50) NOT NULL )
+	(	id int	PRIMARY KEY IDENTITY (1, 1),
+		name nvarchar(max) NOT NULL )
 
 CREATE TABLE Tasks
-	(	id int PRIMARY KEY IDENTITY,
-		name nvarchar(50) NOT NULL,
-		description nvarchar(200) NULL,
-		complete bit NOT NULL,
-		deadline date NOT NULL,
-		project int NOT NULL )
+	(	id int PRIMARY KEY IDENTITY (1, 1),
+		name nvarchar(max) NOT NULL,
+		description nvarchar(max) NULL,
+		completed bit NOT NULL,
+		deadline datetime2 NOT NULL,
+		project_id int NOT NULL )
 
 CREATE TABLE Tags
-	(	id int PRIMARY KEY IDENTITY,
-		name nvarchar(20) NOT NULL )
-
--- Из-за уникальности имен тегов мы не можем сделать прямую привязку тегов к задачам по их айди,
--- поэтому используем небольшой хак чтобы решить проблему и добавляем новую таблицу, в которой
--- будут указаны связи id задач с id их тегов.
+	(	id int PRIMARY KEY IDENTITY (1, 1),
+		name nvarchar(max) NOT NULL )
 
 CREATE TABLE AnchorTags
-	(	id int PRIMARY KEY IDENTITY,
-		task int NOT NULL,
-		tag int NOT NULL )
+	(	id int PRIMARY KEY IDENTITY (1, 1),
+		task_id int NOT NULL,
+		tag_id int NOT NULL )
