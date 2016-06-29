de959167d73998a0361baf538fd124d1adf893dc
SharkovMaksimUrievich <makssharkov@yandex.ru>
2016-06-29 16:57:31 +0300

    Fix SQL12

--- Database/Task_12.sql
+++ Database/Task_12.sql
@@ -1,9 +1,17 @@
 INSERT INTO Projects (name)
 VALUES ('Список задач')
 
+DECLARE @ProjId int;
+SELECT @ProjId = id
+					FROM Projects
+					WHERE name LIKE 'Список задач'
+
 INSERT INTO Tasks (name, completed, project_id)
-SELECT pr.name, 0, pr.id
+SELECT pr.name, 0, @ProjId 
 FROM Projects pr
 WHERE pr.name NOT IN
 	(SELECT ts.name
-		FROM Tasks ts)
+		FROM Tasks ts) OR pr.name IN 
+								(SELECT ts.name
+									FROM Tasks ts
+									WHERE ts.project_id NOT LIKE @ProjId)
