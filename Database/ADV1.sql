52bc80ee68984603a45b6192f85bc9ca1a2712ab
SharkovMaksimUrievich <makssharkov@yandex.ru>
2016-06-30 13:42:24 +0300

    Complete ADV1

--- Database/ADV1.sql
+++ Database/ADV1.sql
@@ -1,6 +1,9 @@
-SELECT tb.name AS Table_Name, ind.name AS Index_Name, prt.row_count AS Index_Row_Count, prt.reserved_page_count AS Total_Size, 
-		prt.used_page_count AS Used_Size, (prt.reserved_page_count - prt.used_page_count) AS Unused_Size
-FROM sys.objects obj
-JOIN sys.tables tb ON tb.object_id = obj.object_id
-JOIN sys.indexes ind ON ind.object_id = obj.object_id
-JOIN sys.dm_db_partition_stats prt ON prt.object_id = obj.object_id
+SELECT tb.name AS Table_Name
+		,ind.name AS Index_Name
+		,prt.row_count AS Index_Row_Count
+		,prt.reserved_page_count * 8 AS Total_Size
+		,prt.used_page_count * 8 AS Used_Size
+		,(prt.reserved_page_count - prt.used_page_count) * 8 AS Unused_Size
+FROM sys.tables tb
+INNER JOIN sys.indexes ind ON ind.object_id = tb.object_id
+INNER JOIN sys.dm_db_partition_stats prt ON (prt.object_id = ind.object_id AND ind.index_id = prt.index_id)
